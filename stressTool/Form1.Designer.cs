d32ef3906a16353940c776c23c8d454eb8ecc578
Ali Akbulut <98882287+AliAkbulut35@users.noreply.github.com>
2022-08-20 03:04:10 +0300

    C# ile Yaptığım Web Stress Tool
    C# Dile kullanarak yaptığım bu Web Servisi Test aracı kullanıcıdan gelen paketlerin Web Servisini ne kadar zorladığını görebilirsiniz

---
+++ stressTool/Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿
+namespace stressTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.hedefUrl = new System.Windows.Forms.Label();
+            this.kullanicisayisilabel = new System.Windows.Forms.Label();
+            this.urlGir = new System.Windows.Forms.TextBox();
+            this.ortalamaiSteklabel = new System.Windows.Forms.Label();
+            this.ortalamaHizlabel = new System.Windows.Forms.Label();
+            this.toplamVerilabel = new System.Windows.Forms.Label();
+            this.Birakilanisteklabel = new System.Windows.Forms.Label();
+            this.toplamZamanlabel = new System.Windows.Forms.Label();
+            this.toplamTaleplabel = new System.Windows.Forms.Label();
+            this.saniyedeGonderileniSteklabel = new System.Windows.Forms.Label();
+            this.baslaButton = new System.Windows.Forms.Button();
+            this.sıfırlaButton = new System.Windows.Forms.Button();
+            this.bitirButton = new System.Windows.Forms.Button();
+            this.kullaniciSayisi = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.kullaniciSayisi)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // hedefUrl
+            // 
+            this.hedefUrl.AutoSize = true;
+            this.hedefUrl.Location = new System.Drawing.Point(52, 38);
+            this.hedefUrl.Name = "hedefUrl";
+            this.hedefUrl.Size = new System.Drawing.Size(51, 13);
+            this.hedefUrl.TabIndex = 0;
+            this.hedefUrl.Text = "URL GİR";
+            // 
+            // kullanicisayisilabel
+            // 
+            this.kullanicisayisilabel.AutoSize = true;
+            this.kullanicisayisilabel.Location = new System.Drawing.Point(52, 85);
+            this.kullanicisayisilabel.Name = "kullanicisayisilabel";
+            this.kullanicisayisilabel.Size = new System.Drawing.Size(76, 13);
+            this.kullanicisayisilabel.TabIndex = 1;
+            this.kullanicisayisilabel.Text = "Kullanıcı Sayısı";
+            // 
+            // urlGir
+            // 
+            this.urlGir.Location = new System.Drawing.Point(150, 35);
+            this.urlGir.Name = "urlGir";
+            this.urlGir.Size = new System.Drawing.Size(267, 20);
+            this.urlGir.TabIndex = 3;
+            this.urlGir.Text = "http://localhost";
+            // 
+            // ortalamaiSteklabel
+            // 
+            this.ortalamaiSteklabel.AutoSize = true;
+            this.ortalamaiSteklabel.Location = new System.Drawing.Point(147, 276);
+            this.ortalamaiSteklabel.Name = "ortalamaiSteklabel";
+            this.ortalamaiSteklabel.Size = new System.Drawing.Size(175, 13);
+            this.ortalamaiSteklabel.TabIndex = 22;
+            this.ortalamaiSteklabel.Text = "Saniyedeki Ortalama İstek Sayısı : 0";
+            this.ortalamaiSteklabel.Click += new System.EventHandler(this.AverageRequestsLabel_Click);
+            // 
+            // ortalamaHizlabel
+            // 
+            this.ortalamaHizlabel.AutoSize = true;
+            this.ortalamaHizlabel.Location = new System.Drawing.Point(147, 248);
+            this.ortalamaHizlabel.Name = "ortalamaHizlabel";
+            this.ortalamaHizlabel.Size = new System.Drawing.Size(111, 13);
+            this.ortalamaHizlabel.TabIndex = 21;
+            this.ortalamaHizlabel.Text = "Ortalama İletim Hızı : 0";
+            // 
+            // toplamVerilabel
+            // 
+            this.toplamVerilabel.AutoSize = true;
+            this.toplamVerilabel.Location = new System.Drawing.Point(145, 226);
+            this.toplamVerilabel.Name = "toplamVerilabel";
+            this.toplamVerilabel.Size = new System.Drawing.Size(110, 13);
+            this.toplamVerilabel.TabIndex = 20;
+            this.toplamVerilabel.Text = "Alınan Toplam Veri : 0";
+            // 
+            // Birakilanisteklabel
+            // 
+            this.Birakilanisteklabel.AutoSize = true;
+            this.Birakilanisteklabel.Location = new System.Drawing.Point(147, 204);
+            this.Birakilanisteklabel.Name = "Birakilanisteklabel";
+            this.Birakilanisteklabel.Size = new System.Drawing.Size(250, 13);
+            this.Birakilanisteklabel.TabIndex = 19;
+            this.Birakilanisteklabel.Text = "Sunucu Tarafından Bırakılan Toplam İstek Sayısı : 0";
+            // 
+            // toplamZamanlabel
+            // 
+            this.toplamZamanlabel.AutoSize = true;
+            this.toplamZamanlabel.Location = new System.Drawing.Point(147, 181);
+            this.toplamZamanlabel.Name = "toplamZamanlabel";
+            this.toplamZamanlabel.Size = new System.Drawing.Size(82, 13);
+            this.toplamZamanlabel.TabIndex = 18;
+            this.toplamZamanlabel.Text = "Toplam Süre : 0";
+            // 
+            // toplamTaleplabel
+            // 
+            this.toplamTaleplabel.AutoSize = true;
+            this.toplamTaleplabel.Location = new System.Drawing.Point(147, 158);
+            this.toplamTaleplabel.Name = "toplamTaleplabel";
+            this.toplamTaleplabel.Size = new System.Drawing.Size(137, 13);
+            this.toplamTaleplabel.TabIndex = 17;
+            this.toplamTaleplabel.Text = "Gönderilen Toplam İstek : 0";
+            // 
+            // saniyedeGonderileniSteklabel
+            // 
+            this.saniyedeGonderileniSteklabel.AutoSize = true;
+            this.saniyedeGonderileniSteklabel.Location = new System.Drawing.Point(147, 130);
+            this.saniyedeGonderileniSteklabel.Name = "saniyedeGonderileniSteklabel";
+            this.saniyedeGonderileniSteklabel.Size = new System.Drawing.Size(236, 13);
+            this.saniyedeGonderileniSteklabel.TabIndex = 16;
+            this.saniyedeGonderileniSteklabel.Text = "Saniyede Gönderilen İstek Hızında Gönderme : 0";
+            // 
+            // baslaButton
+            // 
+            this.baslaButton.Location = new System.Drawing.Point(492, 38);
+            this.baslaButton.Name = "baslaButton";
+            this.baslaButton.Size = new System.Drawing.Size(75, 23);
+            this.baslaButton.TabIndex = 23;
+            this.baslaButton.Text = "Başla";
+            this.baslaButton.UseVisualStyleBackColor = true;
+            this.baslaButton.Click += new System.EventHandler(this.baslaButton_Click);
+            // 
+            // sıfırlaButton
+            // 
+            this.sıfırlaButton.Location = new System.Drawing.Point(492, 85);
+            this.sıfırlaButton.Name = "sıfırlaButton";
+            this.sıfırlaButton.Size = new System.Drawing.Size(75, 23);
+            this.sıfırlaButton.TabIndex = 24;
+            this.sıfırlaButton.Text = "Sıfırla";
+            this.sıfırlaButton.UseVisualStyleBackColor = true;
+            this.sıfırlaButton.Click += new System.EventHandler(this.sıfırlaButton_Click);
+            // 
+            // bitirButton
+            // 
+            this.bitirButton.Location = new System.Drawing.Point(492, 130);
+            this.bitirButton.Name = "bitirButton";
+            this.bitirButton.Size = new System.Drawing.Size(75, 23);
+            this.bitirButton.TabIndex = 25;
+            this.bitirButton.Text = "Bitir";
+            this.bitirButton.UseVisualStyleBackColor = true;
+            this.bitirButton.Click += new System.EventHandler(this.bitirButton_Click);
+            // 
+            // kullaniciSayisi
+            // 
+            this.kullaniciSayisi.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F);
+            this.kullaniciSayisi.Location = new System.Drawing.Point(150, 71);
+            this.kullaniciSayisi.Maximum = new decimal(new int[] {
+            500,
+            0,
+            0,
+            0});
+            this.kullaniciSayisi.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.kullaniciSayisi.Name = "kullaniciSayisi";
+            this.kullaniciSayisi.Size = new System.Drawing.Size(255, 35);
+            this.kullaniciSayisi.TabIndex = 26;
+            this.kullaniciSayisi.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(615, 315);
+            this.Controls.Add(this.kullaniciSayisi);
+            this.Controls.Add(this.bitirButton);
+            this.Controls.Add(this.sıfırlaButton);
+            this.Controls.Add(this.baslaButton);
+            this.Controls.Add(this.ortalamaiSteklabel);
+            this.Controls.Add(this.ortalamaHizlabel);
+            this.Controls.Add(this.toplamVerilabel);
+            this.Controls.Add(this.Birakilanisteklabel);
+            this.Controls.Add(this.toplamZamanlabel);
+            this.Controls.Add(this.toplamTaleplabel);
+            this.Controls.Add(this.saniyedeGonderileniSteklabel);
+            this.Controls.Add(this.urlGir);
+            this.Controls.Add(this.kullanicisayisilabel);
+            this.Controls.Add(this.hedefUrl);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.kullaniciSayisi)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label hedefUrl;
+        private System.Windows.Forms.Label kullanicisayisilabel;
+        private System.Windows.Forms.TextBox urlGir;
+        private System.Windows.Forms.Label ortalamaiSteklabel;
+        private System.Windows.Forms.Label ortalamaHizlabel;
+        private System.Windows.Forms.Label toplamVerilabel;
+        private System.Windows.Forms.Label Birakilanisteklabel;
+        private System.Windows.Forms.Label toplamZamanlabel;
+        private System.Windows.Forms.Label toplamTaleplabel;
+        private System.Windows.Forms.Label saniyedeGonderileniSteklabel;
+        private System.Windows.Forms.Button baslaButton;
+        private System.Windows.Forms.Button sıfırlaButton;
+        private System.Windows.Forms.Button bitirButton;
+        private System.Windows.Forms.NumericUpDown kullaniciSayisi;
+    }
+}
+
---
+++ stressTool/Form1.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Timers;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace stressTool
+{
+    public partial class Form1 : Form
+    {
+        private static long isteklerSaniye = 0;
+        private static long birakilaniStekler = 0;
+        private static long gonderilenToplamistek = 0;
+        private static long toplamAlinanBant = 0;
+        private static System.Timers.Timer aTimer;
+        private static string Url { set; get; }
+        private DateTime baslangicSaat { get; set; }
+        private DateTime bitisZaman { get; set; }
+        CancellationTokenSource cts = new CancellationTokenSource();
+        ParallelOptions po = new ParallelOptions();
+
+        private void testiCalistir()
+        {
+            HttpClient client = new HttpClient();
+            aTimer = new System.Timers.Timer(1000);
+            aTimer.Elapsed += OnTimedEvent;
+            aTimer.AutoReset = true;
+            aTimer.Enabled = true;
+            try
+            {
+                
+                Parallel.For(0, Int32.Parse(kullaniciSayisi.Value.ToString()), async ctr =>
+                {
+                    while (true)
+                    {
+                        Task<bool> dwnld = ProcessUrl(client);
+                        bool z = await dwnld;
+                    }
+                });
+            }
+            catch (OperationCanceledException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                cts.Dispose();
+            }
+
+        }
+
+        private async Task<bool> ProcessUrl(HttpClient client)
+        {
+            try
+            {
+                isteklerSaniye++;
+                gonderilenToplamistek++;
+                var xyz = await client.GetAsync(Url);
+                if (xyz.IsSuccessStatusCode)
+                {
+                    var conten = await xyz.Content.ReadAsStringAsync();
+                    toplamAlinanBant += conten.Length;
+                }
+                else
+                {
+                    Console.WriteLine("Status Code: " + xyz.StatusCode);
+                    birakilaniStekler++;
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                birakilaniStekler++;
+                return false;
+            }
+        }
+
+        delegate void StringArgReturningVoidDelegate(string text);
+
+        private void SetText(string text)
+        {
+            if (this.saniyedeGonderileniSteklabel.InvokeRequired)
+            {
+                StringArgReturningVoidDelegate d = new StringArgReturningVoidDelegate(SetText);
+                this.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                this.saniyedeGonderileniSteklabel.Text = text;
+            }
+        }
+
+        private void OnTimedEvent(object sender, ElapsedEventArgs e)
+        {
+            this.SetText("Saniyedeki istek " + isteklerSaniye + " hızında gönderme");
+            isteklerSaniye = 0;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void ResetApp()
+        {
+            saniyedeGonderileniSteklabel.Text = "Saniyede 0 istek hızında gönderme";
+            saniyedeGonderileniSteklabel.Visible = false;
+            toplamTaleplabel.Text = "Gönderilen Toplam İstek: 0";
+            toplamTaleplabel.Visible = false;
+            toplamZamanlabel.Text = "Toplam Süre: 0s";
+            toplamZamanlabel.Visible = false;
+            Birakilanisteklabel.Text = "Sunucu Tarafından Bırakılan Toplam İstekler: 0";
+            Birakilanisteklabel.Visible = false;
+            toplamVerilabel.Text = "Alınan Toplam Veri: 0 MB";
+            toplamVerilabel.Visible = false;
+            ortalamaHizlabel.Text = "Ortalama İletim Hızı: 0 MB/s";
+            ortalamaHizlabel.Visible = false;
+            ortalamaiSteklabel.Text = "Saniyedeki Ortalama İstek Sayısı: 0";
+            ortalamaiSteklabel.Visible = false;
+            baslaButton.Visible = true;
+            baslaButton.Enabled = true;
+            bitirButton.Visible = true;
+            bitirButton.Enabled = false;
+            sıfırlaButton.Visible = true;
+            sıfırlaButton.Enabled = true;
+            //İptal Simgesi Yapılandırması
+            cts = new CancellationTokenSource();
+            po = new ParallelOptions
+            {
+                CancellationToken = cts.Token
+            };
+            po.MaxDegreeOfParallelism = System.Environment.ProcessorCount * 2;
+
+            Url = "";
+            birakilaniStekler = 0;
+            isteklerSaniye = 0;
+            toplamAlinanBant = 0;
+            gonderilenToplamistek = 0;
+        }
+        //public Form1()
+        //{
+        //    InitializeComponent();
+        //}
+
+       
+
+        
+
+       
+
+        private void AverageRequestsLabel_Click(object sender, EventArgs e)
+        {
+            return;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ResetApp();
+        }
+
+        private void baslaButton_Click(object sender, EventArgs e)
+        {
+            baslaButton.Enabled = false;
+            sıfırlaButton.Enabled = false;
+            bool result = Uri.TryCreate(urlGir.Text, UriKind.Absolute, out Uri uriResult)
+                && (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps);
+            if (!result)
+            {
+                System.Windows.Forms.MessageBox.Show("URL: http://www.FullDomainName or https://www.FullDomainName");
+                baslaButton.Enabled = true;
+                sıfırlaButton.Enabled = true;
+                return;
+            }
+            if (kullaniciSayisi.Value < 1 || kullaniciSayisi.Value > 500)
+            {
+                System.Windows.Forms.MessageBox.Show("Kullanılacak Kullanıcı Sayısı (0,501)" + kullaniciSayisi.Value);
+                baslaButton.Enabled = true;
+                sıfırlaButton.Enabled = true;
+                return;
+            }
+            Url = urlGir.Text;
+            birakilaniStekler = 0;
+            isteklerSaniye = 0;
+            saniyedeGonderileniSteklabel.Visible = true;
+            bitirButton.Enabled = true;
+            baslangicSaat = DateTime.Now;
+            testiCalistir();
+        }
+
+        private void bitirButton_Click(object sender, EventArgs e)
+        {
+            bitirButton.Enabled = false;
+
+            
+
+            bitisZaman = DateTime.Now;
+            var TimeTaken = (bitisZaman.Subtract(baslangicSaat)).TotalSeconds;
+            saniyedeGonderileniSteklabel.Visible = false;
+            toplamTaleplabel.Text = "Gönderilen Toplam İstek: " + gonderilenToplamistek;
+            toplamTaleplabel.Visible = true;
+            toplamZamanlabel.Text = "Toplam Süre: " + $"{TimeTaken:0.#}" + " saniye";
+            toplamZamanlabel.Visible = true;
+            Birakilanisteklabel.Text = "Sunucu Tarafından Bırakılan Toplam İstekler: " + birakilaniStekler;
+            Birakilanisteklabel.Visible = true;
+            toplamVerilabel.Text = $"Alınan Toplam Veri: {(toplamAlinanBant / 1048576.0):0.##} MB";
+            toplamVerilabel.Visible = true;
+            ortalamaHizlabel.Text = $"Ortalama İletim Hızı: {((toplamAlinanBant / 1048576.0) / TimeTaken):0.##} MB/s";
+            ortalamaHizlabel.Visible = true;
+            ortalamaiSteklabel.Text = $"Saniyede Gönderilen İstekler: {(gonderilenToplamistek / TimeTaken):0.##}";
+            ortalamaiSteklabel.Visible = true;
+            sıfırlaButton.Visible = true;
+            sıfırlaButton.Enabled = true;
+            bitirButton.Visible = true;
+            bitirButton.Enabled = false;
+        }
+
+        private void sıfırlaButton_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+        }
+    }
+}
